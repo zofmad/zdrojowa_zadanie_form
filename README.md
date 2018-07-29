5e96a308c119fb45f5dd054f2dce46950974efe3
Zofia Madej <zofia.madej@polcode.net>
2018-07-29 14:07:39 +0200

    create wordpress project, adding theme, form added on the home site, styling the form

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # zdrojowa_zadanie_form
+
+
+
+
+PHP Version 7.1.7
+
+$ node --version
+v8.9.1
+$ npm --v
+6.2.0
+
+to run project:
+1.git clone https://github.com/zofmad/zdrojowa_zadanie_form.git
+2.cd wp-content/themes/theme
+3.npm install
+3.npm run webpack
+
+
+panel admina
+
+login:admin
+hasło:dnjcpQSLdesHp35sb2
+
+w .htaccess są wpisy potrzebne do prawidłowego działania assetów(są poza częścią nadpisywaną przez wordpress)
---
+++ wp-content/plugins/my-plugin/my-plugin.php
@@ -0,0 +1,37 @@
+<?php
+/*
+  Plugin Name: My Plugin
+ */
+
+function createForm() {
+    ?>
+    <form class="contact-form" action="" method="post">
+        <div class="row">
+            <div class="col-xs-12 form_spacer"></div>
+            <div class="col-xs-12 col-sm-6">
+                <div class="form-group">Imię
+                    <input type="text" name="name" class="name form-control" placeholder="Imię">
+                </div>
+                <div class="form-group">Nazwisko
+                    <input type="text" name="surname" class="surname form-control" placeholder="Nazwisko">
+                </div>
+                <div class="form-group">Email*
+                    <input type="email" name="email" class="email form-control" placeholder="Email*" required>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <div class="form-group">Treść wiadomości*
+                    <textarea name="message" class="message form-control"  placeholder="Treść wiadomości*" required></textarea>
+                </div>
+            </div>
+            <div class="col-xs-12">
+                <input type="text" name="field" class="hide">
+                <input type="submit" name="send" class="send text-uppercase" value="Wyślij">
+            </div>
+        </div>
+    </form>
+    <?php
+    return;
+}
+
+add_shortcode('formularz_kontaktowy', 'createForm');
